5b2e44124785cd554240ad0da346344e085d3515
Ejiofor Joshua <ejioforjo01@gmail.com>
2026-08-01 09:06:31 +0100

    country code & sidebar updated

--- src/pages/auth/Signup4.tsx
+++ src/pages/auth/Signup4.tsx
@@ -122,7 +122,12 @@ const Signup4: React.FC = () => {
             label="Phone number"
             placeholder="Enter your phone number"
             value={formData.phoneNumber}
-            onChange={(e) => setFormData({ ...formData, phoneNumber: e.target.value })}
+            onChange={(phoneNumber) =>
+              setFormData({
+                ...formData,
+                phoneNumber,
+              })
+            }
             error={errors.phoneNumber}
           />
 
